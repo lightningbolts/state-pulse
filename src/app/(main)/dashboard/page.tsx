5c785d98a93a49dc171fe01be7634b2ec00b8257
Kairui Cheng <timberlake2025@gmail.com>
2025-08-28 11:11:52 -0700

    Fixed suspense dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import DashboardPageClient from "@/app/(main)/dashboard/DashboardPageClient";
 import { pageMetadata } from '@/lib/metadata';
+import { Suspense } from 'react';
 
 export const metadata = pageMetadata.dashboard;
 
 export default function DashboardPage() {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <DashboardPageClient />
+    </Suspense>
   );
 }
